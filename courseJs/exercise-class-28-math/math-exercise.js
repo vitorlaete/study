dd370a2138d21313a027c9014c1711ecbe339d30
Vitor Laete <123130828+vitorlaete@users.noreply.github.com>
2023-12-23 23:58:01 -0400

    Number exercise

--- courseJs/exercise-class-28-math/math-exercise.js
+++ courseJs/exercise-class-28-math/math-exercise.js
@@ -9,23 +9,59 @@ Get a number, and show:
 * Show the number with two decimal places
 */
 
-// document.addEventListener(function (element) {
-//     const wElement = element.target;
-    
-// });
+const buttonSubmit = document.getElementById("submitButton");
+const inputText = document.getElementById("entryNumber");
+const divField = document.getElementById("field");
 
-const divParent = document.getElementById("field");
+document.addEventListener("click", function (element){
+    if (element.target === buttonSubmit) {
+        showResults();
+    }
+});
 
-function fInputText () {
-    const inputText = document.createElement("input");
-    inputText.setAttribute("type", "text");
-    inputText.setAttribute("id", "boxInformation");
-    inputText.setAttribute("placeholder", "Type your number here");
-    
-    divParent.appendChild(inputText);
+inputText.addEventListener("keypress", function (event) {
+    if (event.key === 'Enter') {
+        showResults();
+    }
+});
+
+function cH1 () {
+    const h1 = document.createElement("h1");
+    h1.setAttribute("id", "divH1");
+    return h1;
 }
 
-const h1 = document.createElement("h1");
-h1.innerHTML = `Seu número é: ${}`;
+function cP () {
+    const p = document.createElement("p");
+    p.setAttribute("class", "divP");
+    return p;
+}
 
-divParent.appendChild(h1);
+function showResults () {
+    divField.appendChild(cH1());
+    const divH1 = document.querySelector("#divH1");
+    divH1.innerHTML = " ";
+
+    divField.appendChild(cP());
+    const p = document.querySelector(".divP");
+    p.innerHTML = " " ;
+
+    divH1.innerHTML += `Your typed number is: <strong>${treatNumbers()}</strong>. <br>`;
+    p.innerHTML += `The square root of the number ${treatNumbers()} is: <strong>${treatNumbers() ** 0.5}</strong>. <br>`;
+    p.innerHTML += `The number ${treatNumbers()} is integer? <strong>${Number.isInteger(treatNumbers())}</strong>. <br>`;
+    p.innerHTML += `The number is a Nan? <strong>${isNaN(treatNumbers())}</strong>. <br>`;
+    p.innerHTML += `The number round to floor is: <strong>${Math.floor(treatNumbers())}</strong>. <br>`;
+    p.innerHTML += `The number round to ceil is: <strong>${Math.ceil(treatNumbers())}</strong>. <br>`;
+    p.innerHTML += `The number with two decimal places is: <strong>${treatNumbers().toFixed(2)}</strong>. <br>`;
+}
+
+function treatNumbers () {
+    const character = inputText.value;
+    if (isNaN(character * 1) === true ) {
+        inputText.focus();
+        inputText.value = "";
+        return "This is not a number. Try again.";
+    } else {
+        return Number(character);
+    }
+}
