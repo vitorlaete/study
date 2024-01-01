d803d5f21c91c08ec001738e88e446a44a34bc86
Vitor Laete <123130828+vitorlaete@users.noreply.github.com>
2024-01-01 17:43:11 -0400

    Added bmi title, bmi grade and index, add calculate title and inputs on the page. Initialiing set button calculate.

---
+++ courseJs/exercise-class-44-IMC/script/imc-script.js
@@ -0,0 +1,10 @@
+
+
+function effectButtonCalculate () {
+    const button = document.querySelector(".buttonCalculate");
+
+    const body = document.body;
+    body.addEventListener("onmouseenter", (element) => {
+
+    })
+}
