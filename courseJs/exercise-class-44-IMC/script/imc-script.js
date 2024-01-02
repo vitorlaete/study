e8e45fe597b2aed701c7e83dbaaf14a886ba4021
Vitor Laete <123130828+vitorlaete@users.noreply.github.com>
2024-01-01 21:20:49 -0400

    added formula of bmi, visual for if and conditions.

--- courseJs/exercise-class-44-IMC/script/imc-script.js
+++ courseJs/exercise-class-44-IMC/script/imc-script.js
@@ -1,10 +1,48 @@
-
-
 function effectButtonCalculate () {
     const button = document.querySelector(".buttonCalculate");
 
-    const body = document.body;
-    body.addEventListener("onmouseenter", (element) => {
+    button.addEventListener("mouseover", () => {
+        button.classList.add("over");
+    });
+    
+    button.addEventListener("mouseout", () => {
+        button.classList.remove("over");
+    });
 
-    })
+    button.addEventListener("click", () => {
+        button.classList.add("clicking");
+    });
+
+    button.addEventListener("mouseout", () => {
+        button.classList.remove("clicking");
+    });
+}
+
+effectButtonCalculate();
+
+function inputWeight () {
+    const weight = document.querySelector("#weight");
+
+    return Number(weight.value);
+}
+
+function inputHeight () {
+    const height = document.querySelector("#height");
+
+    return Number(height.value);
+}
+
+function calculateBMI (weight, height) {
+    const result = document.querySelector("#result");
+
+    const resultBMI = weight / (height* height);
+
+    const table = [18.5, 24.9, 29.9, 34.9, 39.9, 40];
+
+    const imcResult = ["Underweight", "Normal weight", "Overweight", "Class I Obesity", "Class II Obesity", "Class III Obesity"];
+
+    if (resultBMI < table[0]) {
+        result.innerHTML = `BMI = ${resultBMI.toFixed(2)} kg/m² (${imcResult[0]})`;
+        result.classList.add("red");
+    } 
 }
