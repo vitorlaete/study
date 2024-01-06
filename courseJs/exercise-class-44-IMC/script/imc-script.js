d256e330edfe0efbe91e9a5443a0165749e55104
Vitor Laete <123130828+vitorlaete@users.noreply.github.com>
2024-01-06 10:37:45 -0400

    finished exercise in js about bmi calculator.

--- courseJs/exercise-class-44-IMC/script/imc-script.js
+++ courseJs/exercise-class-44-IMC/script/imc-script.js
@@ -19,7 +19,15 @@ function eventButtonCalculate () {
     });
 }
 
+
+
+
+
 eventButtonCalculate();
+
+
+
+
 
 function inputWeight () {
     const weight = document.querySelector("#weight");
@@ -27,25 +35,43 @@ function inputWeight () {
     return Number(weight.value);
 }
 
-function inputHeight () {
 
+
+
+
+function inputHeight () {
     const height = document.querySelector("#height");
 
     return Number(height.value);
 }
 
+
+
+
 function calculateBMI (weight, height) {
 
     clear();
-
+    
     const result = document.querySelector("#result");
-
+    
     const resultBMI = weight / (height* height);
-
+    
     const table = [18.5, 24.9, 29.9, 34.9, 39.9, 40];
-
+    
     const imcResult = ["Underweight", "Normal weight", "Overweight", "Class I Obesity", "Class II Obesity", "Class III Obesity"];
-
+    
+    
+        if (weight === 0 && height === 0) {    
+            result.classList.add("red");
+            return result.innerHTML = "Invalid values.";
+        } else if (weight === 0) {
+            result.classList.add("red");
+            return result.innerHTML = "Weight is invalid.";
+        } else if (height === 0) {
+            result.classList.add("red");
+            return result.innerHTML = "Height is invalid.";
+        }
+    
     if (resultBMI < table[0]) {
         result.innerHTML = `BMI = ${resultBMI.toFixed(2)} kg/m² (${imcResult[0]})`;
         result.classList.add("red");
@@ -61,7 +87,7 @@ function calculateBMI (weight, height) {
     } else if (resultBMI < table[4]) {
         result.innerHTML = `BMI = ${resultBMI.toFixed(2)} kg/m² (${imcResult[4]})`;
         result.classList.add("red");
-    } else if (resultBMI < table[5]) {
+    } else if (resultBMI > table[5]) {
         result.innerHTML = `BMI = ${resultBMI.toFixed(2)} kg/m² (${imcResult[5]})`;
         result.classList.add("red");
     }
@@ -69,6 +95,7 @@ function calculateBMI (weight, height) {
 
 function clear () {
     document.querySelector("#result").innerHTML = "";
+    document.querySelector("#result").className = "";
     document.querySelector("#weight").value = "";
     document.querySelector("#height").value = "";
 }
